c60e96d9253e87b580a274fdbc1ba6b938aa42fb
jonahvimahi <jonahvimahi@gmail.com>
2021-08-27 10:01:08 -0600

    added automated tests

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -16,5 +16,24 @@ test('I can start a game', async () => {
 
     let button = await (await driver).findElement(By.id('start-game'));
     await button.click();
+
+    await driver.sleep(2000)
     
+    let topLeft = await driver.findElement(By.id('cell-0'))
+    await topLeft.click()
+
+    await driver.sleep(2000)
+
+    // let topMiddle = await driver.findElement(By.id("cell-1"))
+    // await topMiddle
+
+    let topRight = await driver.findElement(By.id("cell-2"))
+    await topRight.click()
+
+    await driver.sleep(2000)
+
+    let bottomRight = await driver.findElement(By.id("cell-8"))
+    await bottomRight.click()
+
+    await driver.sleep(2000)
 });
